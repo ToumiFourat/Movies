fa35db44a07425c5a6dc0452112875a8f70b5d81
ToumiFourat <fourattoumi021@gmail.com>
2025-02-08 19:26:27 +0100

    Creating Authentication UI Part 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Navbar from './components/user/Navbar'
+import Signin from './components/auth/Signin'
 
 export default function App() {
   return (
-    <div><Navbar/> </div>
+    <div> <>
+        <Navbar/>
+        <Signin/>
+      </>  </div>
   )
 }
---
+++ frontend/src/components/auth/Signin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Container from "../Container";
+import Title from "../form/Title";
+import FormInput from "../form/FormInput";
+import Submit from "../form/submit";
+
+export default function Signin() {
+  return (
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-72 space-y-6">
+          <Title>Sign in</Title>
+          <FormInput label="Email" placeholder="fourattoumi021@gmail.com" />
+          <FormInput label="Password" placeholder="********" name="password" />
+          <Submit value="Sign in" />
+        </form>
+      </Container>
+    </div>
+  );
+}

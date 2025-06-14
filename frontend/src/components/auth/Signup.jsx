b025b525f119ad639875a165b70fbd4310eec30e
ToumiFourat <fourattoumi021@gmail.com>
2025-06-14 19:53:10 +0100

    Validating Signup Form

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
@@ -6,21 +6,69 @@ import Submit from "../form/Submit";
 import CustomLink from "../CustomLink";
 import { commonModalClasses } from "../../utils/Theme";
 import FormContainer from "../form/FormContainer";
+const validateUserInfo = ({ name, email, password }) => {
+  const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const isValidName = /^[a-z A-Z]+$/;
+  if (!name.trim()) return { ok: false, error: "Name is missing!" };
+  if (!isValidName.test(name)) return { ok: false, error: "Invalid name!" };
+  if (!email.trim()) return { ok: false, error: "Email is missing!" };
+  if (!isValidEmail.test(email)) return { ok: false, error: "Invalid email!" };
+  if (!password.trim()) return { ok: false, error: "Password is missing!" };
+  if (password.length < 8)
+    return { ok: false, error: "Password must be 8 characters long!" };
+  return { ok: true };
+};
 
 export default function Signup() {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { ok, error } = validateUserInfo(userInfo);
+
+    if (!ok) return console.log(error);
+    console.log(userInfo);
+  };
+
+  const { name, email, password } = userInfo;
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-72"}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
           <Title>Sign up</Title>
-          <FormInput label="Name" placeholder="Toumi Fourat" name="name" />
           <FormInput
+            value={name}
+            onChange={handleChange}
+            label="Name"
+            placeholder="Toumi Fourat"
+            name="name"
+          />
+          <FormInput
+            vlaue={email}
+            onChange={handleChange}
             label="Email"
             placeholder="fourattoumi021@gmail.com"
             name="email"
           />
-          <FormInput label="Password" placeholder="********" name="password" />
-          <Submit value="Sign in" />
+          <FormInput
+            vlaue={password}
+            onChange={handleChange}
+            label="Password"
+            placeholder="********"
+            name="password"
+            type="password"
+          />
+          <Submit value="Sign up" />
           <div className="flex justify-between">
             <CustomLink to="/auth/forget-password">Forget Password</CustomLink>
             <CustomLink to="/auth/signin">Sign in</CustomLink>

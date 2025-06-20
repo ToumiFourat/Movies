0b7f2b4eda5d57a06a5a1283032ff478764d03eb
ToumiFourat <fourattoumi021@gmail.com>
2025-06-20 22:43:49 +0100

    Method for Forget Password & Handling Form State

--- frontend/src/components/auth/ForgetPassword.jsx
+++ frontend/src/components/auth/ForgetPassword.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Container";
 import Title from "../form/Title";
 import FormInput from "../form/FormInput";
@@ -6,14 +6,37 @@ import CustomLink from "../CustomLink";
 import Submit from "../form/Submit";
 import FormContainer from "../form/FormContainer";
 import { commonModalClasses } from "../../utils/Theme";
+import { forgetPassword } from "../../api/auth";
+import { isValidEmail } from "../../utils/helper";
+import { useNotification } from "../../hooks";
 
 export default function ForgetPassword() {
+  const [email, setEmail] = useState("");
+  const { updateNotification } = useNotification();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isValidEmail(email))
+      return updateNotification("error", "Invalid email!");
+    const { error, message } = await forgetPassword(email);
+    if (error) return updateNotification("error", error);
+
+    updateNotification("success", message);
+  };
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setEmail(value);
+  };
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-96 "}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-96 "}>
           <Title>Please Enter Your Email</Title>
-          <FormInput label="Email" placeholder="fourattoumi021@gmail.com" />
+          <FormInput
+            onChange={handleChange}
+            value={email}
+            label="Email"
+            placeholder="fourattoumi021@gmail.com"
+          />
 
           <Submit value="Send Link" />
           <div className="flex justify-between">
